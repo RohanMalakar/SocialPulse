44f4f765e6af66f199a87ce984accbb084c7b3c3
RohanMalakar <rohanmalakar5091>
2025-01-08 23:15:42 +0530

    get ready to production

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,9 @@
+import dotenv from "dotenv"
+dotenv.config();
 import express from "express"
 import cors from "cors"
 import langflowRoutes from "./routes/LangFlow.route.js"
+import path from "path"
 const app=express();
 
 app.use(cors());
@@ -13,39 +16,50 @@ app.use(express.static("public"));
 
 app.use("/api/v1/langflow", langflowRoutes); // Mount the Langflow routes
 
-app.post('/api/v1/add-post', async (req, res) => {
-  const { type, likes, comments, shares } = req.body;
-  try {
-      await client.execute(
-          'INSERT INTO posts (id, type, likes, comments, shares) VALUES (uuid(), ?, ?, ?, ?)',
-          [type, likes, comments, shares],
-          { prepare: true }
-      );
-      res.status(200).send('Post added successfully!');
-  } catch (err) {
-      res.status(500).send('Error adding post: ' + err.message);
-  }
-});
+// app.post('/api/v1/add-post', async (req, res) => {
+//   const { type, likes, comments, shares } = req.body;
+//   try {
+//       await client.execute(
+//           'INSERT INTO posts (id, type, likes, comments, shares) VALUES (uuid(), ?, ?, ?, ?)',
+//           [type, likes, comments, shares],
+//           { prepare: true }
+//       );
+//       res.status(200).send('Post added successfully!');
+//   } catch (err) {
+//       res.status(500).send('Error adding post: ' + err.message);
+//   }
+// });
 
-app.get('/api/v1/get-posts', async (req, res) => {
-  try {
-      const result = await client.execute('SELECT * FROM posts');
-      res.status(200).json(result.rows);
-  } catch (err) {
-      res.status(500).send('Error fetching posts: ' + err.message);
-  }
-});
+// app.get('/api/v1/get-posts', async (req, res) => {
+//   try {
+//       const result = await client.execute('SELECT * FROM posts');
+//       res.status(200).json(result.rows);
+//   } catch (err) {
+//       res.status(500).send('Error fetching posts: ' + err.message);
+//   }
+// });
+
+
+const  _dirname= path.resolve();
+if (process.env.NODE_ENV==="production") {
+      app.use(express.static(path.join(_dirname, "/frontend/dist")));
+      app.get('*', (req, res) => {
+      res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+  })
+} 
+else 
+{
+      app.get("/", (req, res) => {
+      res.send("API is Running Successfully");
+      });
+}
 
 
 
-app.use("/",(req,res)=>{
-  console.log("Hey I am rohan malakar")
-  res.send("Hey I am rohan malakar")   
-});
 
-app.all("*",(req,res,next)=>{
-      res.status(404)
-      res.send("OOPS! page not found")   
-});
+// app.all("*",(req,res,next)=>{
+//       res.status(404)
+//       res.send("OOPS! page not found")   
+// });
 
 export {app}
