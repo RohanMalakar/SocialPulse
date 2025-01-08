40e3650fe4a4e8ba5d17d7c572e3e518305751fd
RohanMalakar <rohanmalakar5091>
2025-01-09 01:06:15 +0530

    changed

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   build: {
-    outDir: "public", // Change the output directory to "public"
+    outDir: "dist", // Change the output directory to "public"
   },
   plugins: [react()],
 })
